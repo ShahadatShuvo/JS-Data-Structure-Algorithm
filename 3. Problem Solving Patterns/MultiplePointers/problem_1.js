9a7c4d551b74d4b3c5ec30865c74a40402d8c8b5
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-11 16:06:12 +0600

    feat[multiple pointers problem]

--- 3. Problem Solving Patterns/MultiplePointers/problem_1.js
+++ 3. Problem Solving Patterns/MultiplePointers/problem_1.js
@@ -4,11 +4,37 @@
 // sumZero([-2,0,1,3]) // undefined
 // sumZero([1,2,3]) // undefined
 
+// function sumZero(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === 0) {
+//         return [arr[i], arr[j]];
+//       }
+//     }
+//   }
+// }
+
+// let result = sumZero([-6,-3, -2, -1, 0, 1, 2, 3]);
+// console.log(result); // [ -3, 3 ]
+
+// Note: NAIVE SOLUTION
+//  Time Complexity - O(N ^ 2)
+//  Space Complexity - O(1)
+
+// *******************REFACTOR********************
+
 function sumZero(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === 0) {
-        return [arr[i], arr[j]];
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else {
+      if (sum > 0) {
+        right--;
+      } else {
+        left++;
       }
     }
   }
@@ -16,5 +42,3 @@ function sumZero(arr) {
 
 let result = sumZero([-3, -2, -1, 0, 1, 2, 3]);
 console.log(result); // [ -3, 3 ]
-
-// Note: Complexity of this solution is O(n^2)
