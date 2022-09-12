a83de8590bde5fa832bddb8ce61167b33db14186
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-12 13:29:24 +0600

    problem solving

--- 3. Problem Solving Patterns/MultiplePointers/problem_3.js
+++ 3. Problem Solving Patterns/MultiplePointers/problem_3.js
@@ -13,3 +13,22 @@
 // averagePair([1,3,3,5,6,7,10,12,19],8) // true
 // averagePair([-1,0,3,4,5,6], 4.1) // false
 // averagePair([],4) // false
+
+function averagePair(arr, query) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    let sum = arr[left] + arr[right];
+    if (query * 2 === sum) {
+      return true;
+    } else if (sum > query * 2) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return false;
+}
+
+let res = averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8);
+console.log(res);
