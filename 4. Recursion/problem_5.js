608e30e7d720f0b20a38480ee1bbdc8e8add343e
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-16 17:03:16 +0600

    added new file

---
+++ 4. Recursion/problem_5.js
@@ -0,0 +1,26 @@
+function collectOddValues(arr) {
+  let newArr = [];
+
+  if (arr.length === 0) {
+    return newArr;
+  }
+
+  if (arr[0] % 2 !== 0) {
+    newArr.push(arr[0]);
+  }
+
+  // [21, 5, 47, 9, 3].concat(collectOddValues([]));
+  // [21, 5, 47, 9, 3].concat(collectOddValues([]));
+  // [21, 5, 47, 9].concat(collectOddValues([3]));
+  // [21, 5, 47].concat(collectOddValues([9, 3]));
+  // [21, 5].concat(collectOddValues([47, 9, 3]));
+  // [21].concat(collectOddValues([5, 47, 9, 3]));
+  // [21].concat(collectOddValues([8, 5, 47, 9, 3]));
+
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+  console.log(newArr);
+  return newArr;
+}
+
+let res = collectOddValues([21, 8, 5, 47, 9, 3]);
+console.log(res); //[21, 5, 47, 9, 3]
--- 4. Recursion/random.js
+++ 4. Recursion/random.js
@@ -1,51 +1,28 @@
-// Please implement a software for a Coffee House using Object Oriented Programming. If you are not familiar or
-// comfortable with object-oriented programming, we suggest you give this article a quick read to ensure you
-// understand it before you attempt it.
-// It should have the following features:
-// - Allow users to order a coffee from Espresso, Cappuccino, Latte.
-// - Allow users to specify add-ons: Milk, Cream, Latte
-// Prices for the available options are as follows
+// What about big O?
+// Measuring time complexity is relatively simple. You can measure the time complexity of a recursive function as then number of recursive calls you need to make relative to the input
+// Measuring space complexity is a bit more challenging. You can measure the space complexity of a recursive function as the maximum number of functions on the call stack at a given time, since the call stack requires memory.
+// RECURSION
+// PROBLEM SET
+// POWER
+// power(2,4) //16
+// power(3,2) //9
+// power(3,3) //27
 
-// class Coffee {
-//   constructor(user, CofeeType, adOns, price) {
-//     this.user = user;
-//     this.CofeeType = CofeeType;
-//     this.adOns = adOns;
-//     this.price = price;
-//     this.getReceipt = function () {
-//       return `${this.user}`;
-//     };
-//   }
-// }
+// POWER power( 2 , 4 ) //16 power( 3 , 2 ) //9 power( 3 , 3 ) //27 Write a function which accepts a base an an exponent.  It should return the result of raising the base to that exponent.
 
-const cofeeDetails = [
-  {
-    CofeeType: "Espresso",
-    adOns: ["Milk", "Cream", "Latte"],
-    price: {
-      Milk: 60,
-      Cream: 75,
-      Latte: 100,
-    },
-  },
-  {
-    CofeeType: "Cappuccino",
-    adOns: ["Milk", "Cream", "Latte"],
-    price: {
-      Milk: 80,
-      Cream: 90,
-      Latte: 125,
-    },
-  },
-  {
-    CofeeType: "Latte",
-    adOns: ["Milk", "Cream", "Latte"],
-    price: {
-      Milk: 100,
-      Cream: 125,
-      Latte: 150,
-    },
-  },
-];
+function power(base, pow) {
+  if (pow === 0) return 1;
+  return base * power(base, pow - 1);
 
-const order = new Coffee(user, cofeeTypes[0], addOns[0], price);
+  // 2 * power(2, 3)  === 16
+  8;
+  //      2 * power(2, 2);
+  4;
+  //          2 * power(2, 1);
+  2;
+  //                 2 * power(2, 0);
+  //                        1
+}
+
+let res = power(2, 4);
+console.log(res);
