dc4b9a410f6c577d245520f541eac3c5315574f1
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-20 18:44:57 +0600

    bubble sort added

---
+++ 6. Sorting Algorithms/1. Bubble sort.js
@@ -0,0 +1,24 @@
+// BubbleSort
+// A sorting algorithm where the largest values bubble up to the top!
+
+// BubbleSort Pseudocode
+
+// Start looping from with a variable called i the end of the array towards the beginning
+// Start an inner loop with a variable called j from the beginning until i - 1
+// If arr[j] is greater than arr[j+1], swap those two values!
+// Return the sorted array
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]]; // Check Swappin.js file for this line..
+      }
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+
+let res = bubbleSort([5, 3, 4, 1, 2]);
+console.log(res); // [ 1, 2, 3, 4, 5 ]
