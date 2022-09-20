91369179d68178e23ba97e679a123efef2ffa38f
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-20 10:42:57 +0600

    solving leetcode problems

--- problem_solving/Leetcode/189. Rotate Array.js
+++ problem_solving/Leetcode/189. Rotate Array.js
@@ -1,6 +1,9 @@
 var rotate = function (nums, k) {
   let len = nums.length;
   let arr = [];
+  if (k > len) {
+    k = k % len;
+  }
   for (let i = 0; i < len; i++) {
     let temp = i + k;
     if (temp >= len) {
@@ -10,8 +13,11 @@ var rotate = function (nums, k) {
       arr[temp] = nums[i];
     }
   }
-  return arr;
+  for (let i = 0; i < arr.length; i++) {
+    nums[i] = arr[i];
+  }
+  return nums;
 };
 
-let res = rotate([1, 2, 3, 4, 5, 6, 7], 3);
+let res = rotate([1, 2], 5);
 console.log(res);
