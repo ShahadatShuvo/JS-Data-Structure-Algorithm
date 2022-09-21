9005ce51d9c616fc3e1f1a866b679f540fbea07b
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-21 23:18:40 +0600

    studying on merging two sorted array

--- 6. Sorting Algorithms/Merge sort/1. Merge sort.js
+++ 6. Sorting Algorithms/Merge sort/1. Merge sort.js
@@ -2,3 +2,30 @@
 // It's a combination of two things - merging and sorting!
 // Exploits the fact that arrays of 0 or 1 element are always sorted
 // Works by decomposing an array into smaller arrays of 0 or 1 elements, then building up a newly sorted array
+
+function mergeTwoSortedArray(arr1, arr2) {
+  let sortedArr = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      sortedArr.push(arr1[i]);
+      i++;
+    } else {
+      sortedArr.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    sortedArr.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    sortedArr.push(arr2[j]);
+    j++;
+  }
+  return sortedArr;
+}
+
+let res = mergeTwoSortedArray([1, 3, 5, 7, 9], [2, 4, 6, 8, 10]);
+console.log(res);
