d336957a278a064bbbebb393d2e54e9d0057a3cd
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-26 22:21:49 +0600

    Doubly Linked List

--- Data Structures/Doubly Linked Lists.js/practice_2.js
+++ Data Structures/Doubly Linked Lists.js/practice_2.js
@@ -42,6 +42,33 @@ class DoublyLinkedList {
     this.length--;
     return removedVal;
   }
+
+  shift() {
+    if (this.length === 0) return undefined;
+    let shiftedVal = this.head.val;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+    }
+    this.length--;
+    return shiftedVal;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (this.length == 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let list = new DoublyLinkedList();
@@ -55,3 +82,9 @@ console.log(list);
 console.log(list.pop());
 
 console.log(list);
+
+console.log(list.shift());
+
+console.log(list);
+
+console.log(list.unshift(80));
