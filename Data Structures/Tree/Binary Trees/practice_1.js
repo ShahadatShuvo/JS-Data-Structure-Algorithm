25c00c50f6f760185c690ac717c5a07f8e27b88e
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-29 13:04:47 +0600

    studying binary tree

---
+++ Data Structures/Tree/Binary Trees/practice_1.js
@@ -0,0 +1,54 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+    let newNode = new Node(val);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (current) {
+        if (val < current.val) {
+          if (!current.left) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (!current.right) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(20);
+tree.insert(8);
+tree.insert(30);
+console.log(tree);
+
+//               10
+//       5               15
+//  N/A      8       N/A      20
+//       N/A   N/A        N/A    30
