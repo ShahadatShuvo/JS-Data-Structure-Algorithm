aec9f4a64fd28867663cb58e252828d3b464d25d
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-30 17:28:24 +0600

    Binary tree

---
+++ Data Structures/Tree/Binary Trees/practice_3.js
@@ -0,0 +1,54 @@
+class Node {
+  constructor(val, right, left) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+//                10
+//          8            20
+//       5     9      15     30
+//     3   6    11  13     28   40
+
+class BinartTree {
+  constructor() {
+    this.root = null;
+  }
+  insertion(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let current = this.root;
+      while (current) {
+        if (val > current.val) {
+          if (current.right) {
+            current = current.right;
+          }
+          current.right = newNode;
+          return this;
+        } else if (val < current.val) {
+          if (current.left) {
+            current = current.left;
+          }
+          current.left = newNode;
+          return this;
+        } else {
+          return undefined;
+        }
+      }
+    }
+  }
+}
+
+let tree = new BinartTree();
+
+console.log(tree);
+tree.insertion(10);
+tree.insertion(8);
+tree.insertion(20);
+tree.insertion(30);
+tree.insertion(18);
+
+console.log(tree);
