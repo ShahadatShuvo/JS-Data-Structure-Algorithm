48d56c240b6bb033b3862faa08e56e3a5b2edc43
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-10-11 13:47:10 +0600

    solving leetcode problem

---
+++ problem_solving/Leetcode/1351. Count Negative Numbers in a Sorted Matrix.js
@@ -0,0 +1,19 @@
+var countNegatives = function (grid) {
+  let counter = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] < 0) {
+        counter++;
+      }
+    }
+  }
+  return counter;
+};
+
+let res = countNegatives([
+  [4, 3, 2, -1],
+  [3, 2, 1, -1],
+  [1, 1, -1, -2],
+  [-1, -1, -2, -3],
+]);
+console.log(res);
