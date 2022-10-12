d7c571015e66922a23e70a99bced5e4e00c9cedf
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-10-12 18:54:22 +0600

    solving leetcode problem

---
+++ problem_solving/Leetcode/367. Valid Perfect Square.js
@@ -0,0 +1,13 @@
+var isPerfectSquare = function (num) {
+  let i = 1;
+  while (i * i <= num) {
+    if (i * i === num) {
+      return true;
+    }
+    i++;
+  }
+  return false;
+};
+
+let res = isPerfectSquare(14);
+console.log(res);
