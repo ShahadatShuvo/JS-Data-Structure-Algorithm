1a1b010ae715681f81ba436ea805507356917d35
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-10-12 18:25:26 +0600

    solving leetcode problem

--- problem_solving/Leetcode/66. Plus One.js
+++ problem_solving/Leetcode/66. Plus One.js
@@ -1,9 +1,24 @@
 var plusOne = function (digits) {
-  let str = digits.join("").toString();
-  let num = Number(str) + 1;
-  str = String(num);
-  return str.split("");
+  if (digits[digits.length - 1] === 9) {
+    let flag = 0;
+    for (let i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i] += 1;
+        flag = 1;
+        break;
+      } else {
+        digits[i] = 0;
+      }
+    }
+    console.log(flag);
+    if (flag === 0) {
+      digits.unshift(1);
+    }
+  } else {
+    digits[digits.length - 1] += 1;
+  }
+  return digits;
 };
 
-let res = plusOne([1, 2, 3]);
+let res = plusOne([9]);
 console.log(res);
